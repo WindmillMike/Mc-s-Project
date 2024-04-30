4d2bc1e26776826e345c773cf43bcd5585c57b88
WindmillMike <83901089+WindmillMike@users.noreply.github.com>
2024-05-01 02:12:37 +0300

    Add files via upload

--- HTML/JAVASCRIPT/index.js
+++ HTML/JAVASCRIPT/index.js
@@ -1,12 +1,14 @@
 window.addEventListener('scroll', function() {
-  
-      var Logo = document.getElementById('logoWrapper');
+
+      var Logo = document.getElementById('Logo');
+      var logoWrapper = document.getElementById('logoWrapper');
       var menuButton = document.getElementById('menuButton');
       var searchButton = document.getElementById('searchButton');
       var searchIcon = document.getElementById('searchIcon');
   
       if (window.scrollY > 1) {
-        Logo.classList.remove('decenter');
+        logoWrapper.classList.remove('decenter');
+        logoWrapper.classList.add('center');
         Logo.classList.add('center');
         menuButton.classList.remove('dissappear');
         menuButton.classList.add('appear');
@@ -15,8 +17,9 @@ window.addEventListener('scroll', function() {
         searchIcon.classList.remove('dissappear');
         searchIcon.classList.add('appear');
       } else {
+        logoWrapper.classList.remove('center'); 
+        logoWrapper.classList.add('decenter'); 
         Logo.classList.remove('center'); 
-        Logo.classList.add('decenter'); 
         menuButton.classList.remove('appear');
         menuButton.classList.add('dissappear');
         searchButton.classList.remove('appear');
@@ -29,12 +32,21 @@ window.addEventListener('scroll', function() {
 
   var menuWrapper = document.getElementById('menuWrapper');
   var menuParts = document.getElementById('menuParts');
+  var Transparent = document.getElementById('Transparent');
+  var headerWrapper = document.getElementById('headerWrapper');
+  var Body = document.getElementById('Body');
   
   document.getElementById("menuButton").addEventListener("click", function() {
     menuWrapper.classList.remove('close');
     menuParts.classList.remove('close');
     menuWrapper.classList.add('open');
     menuParts.classList.add('open');
+    Transparent.classList.remove('close');
+    Transparent.classList.add('open');
+    headerWrapper.classList.remove('close');
+    headerWrapper.classList.add('open');
+    Body.classList.remove('Move');
+    Body.classList.add('noMove');
   });
 
   document.getElementById("closeButton").addEventListener("click", function() {
@@ -42,4 +54,10 @@ window.addEventListener('scroll', function() {
     menuParts.classList.remove('open');
     menuWrapper.classList.add('close');
     menuParts.classList.add('close');
+    Transparent.classList.remove('open');
+    Transparent.classList.add('close');
+    headerWrapper.classList.remove('open');
+    headerWrapper.classList.add('close');
+    Body.classList.remove('noMove');
+    Body.classList.add('Move');
   });
